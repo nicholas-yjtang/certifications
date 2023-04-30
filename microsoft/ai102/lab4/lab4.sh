c0816c13155d32f2600e3adea13574f7de642b75
Nicholas Tang <nicholas.tang@knocaminolta.com>
2023-04-30 08:09:10 +0800

    removed output so there is no need to chown the output directory for the docker instance

--- microsoft/ai102/lab4/lab4.sh
+++ microsoft/ai102/lab4/lab4.sh
@@ -28,17 +28,18 @@ echo cognitiveServiceName=$cognitiveServiceName
 #get endpoint and key for cognitive service
 export endpoint=$(az cognitiveservices account show --name $cognitiveServiceName --resource-group $resourceGroup --query "properties.endpoint" --output tsv)
 export key=$(az cognitiveservices account keys list --name $cognitiveServiceName --resource-group $resourceGroup --query "key1" --output tsv)
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export output=$SCRIPTPATH/output
-echo $endpoint,$key, $output, $input
+echo $endpoint,$key
 #start docker container and have it run in the background
-if [ ! -d "$output" ]; then
-    mkdir $output
-fi
-#check if output folder is owned by user 999
-if [ ! "$(stat -c %U $output)" = "999" ]; then
-    sudo chown -R 999:999 $output
-fi
 #original example uses language detection, but the docker image has an issue, so changed to using sentiment instead as an example
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+pushd $SCRIPTPATH
 docker compose up -d
-curl -X POST "http://localhost:5000/text/analytics/v3.0/sentiment" -H "Content-Type: application/json" -d @$SCRIPTPATH/request.json
+#check if docker container is running
+status=$(curl -s http://localhost:5000/ready | jq -r .ready)
+echo $status
+if [[ $status != "ready" ]]; then
+    echo "docker container is not running"
+    exit 1
+fi
+curl -X POST "http://localhost:5000/text/analytics/v3.0/sentiment" -H "Content-Type: application/json" -d @request.json
+popd
